4fb80e7232667b9a6147e4d17bce4618ea434921
Sanhapas <sanhapas.arun@gmail.com>
2025-05-25 10:13:48 +0700

    init

---
+++ chat/src/main/java/co/th/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package co.th.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
